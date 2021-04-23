5388c88c3599a92bba2283e2b572a4f0b4940f69
2439 <2439504029@qq.com>
2021-04-23 22:09:41 +0800

    2021/4/23 stereo_match no correct maybe

--- stereo_match/match.h
+++ stereo_match/match.h
@@ -20,11 +20,15 @@ public:
 	int SADWindowSize;
 	int numberOfDisparities;
 
-	cv::Mat disp;
+	cv::Mat disp, disp8;
 	cv::Mat depth;
 	cv::Ptr<cv::StereoBM> bm;
 	cv::Ptr<cv::StereoSGBM> sgbm;
 	cv::Size imgSize;
+
+	float fxMulBase;
+
+	int alg;	// 0:BM, 1:SGBM;
 
 	/*-----------------------------------
 	 * 功能：初始化内部变量，载入双目定标结果，match参数设置
@@ -37,5 +41,5 @@ public:
 	 * 功能：匹配
 	 *-----------------------------------
 	 */
-	int stereo_match(cv::Mat left, cv::Mat right, int x, int y, uchar* depth);
+	int stereo_match(cv::Mat leftFrame, cv::Mat rightFrame, int x, int y, uchar* depthData);
 };
